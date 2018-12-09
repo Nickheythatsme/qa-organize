f902e208495a4e38c60208474f792fadd644670a
Nick <njgrout@gmail.com>
2018-12-09 12:02:34 -0800

    deleted:    package-lock.json 	deleted:    package.json 	deleted:    public/favicon.ico 	deleted:    public/index.html 	deleted:    public/manifest.json 	new file:   qa-organize/.editorconfig 	new file:   qa-organize/.gitignore 	new file:   qa-organize/README.md 	new file:   qa-organize/angular.json 	new file:   qa-organize/e2e/protractor.conf.js 	new file:   qa-organize/e2e/src/app.e2e-spec.ts 	new file:   qa-organize/e2e/src/app.po.ts 	new file:   qa-organize/e2e/tsconfig.e2e.json 	new file:   qa-organize/package-lock.json 	new file:   qa-organize/package.json 	new file:   qa-organize/src/app/app-routing.module.ts 	new file:   qa-organize/src/app/app.component.html 	new file:   qa-organize/src/app/app.component.scss 	new file:   qa-organize/src/app/app.component.spec.ts 	new file:   qa-organize/src/app/app.component.ts 	new file:   qa-organize/src/app/app.module.ts 	new file:   qa-organize/src/assets/.gitkeep 	new file:   qa-organize/src/browserslist 	new file:   qa-organize/src/environments/environment.prod.ts 	new file:   qa-organize/src/environments/environment.ts 	new file:   qa-organize/src/favicon.ico 	new file:   qa-organize/src/index.html 	new file:   qa-organize/src/karma.conf.js 	new file:   qa-organize/src/main.ts 	new file:   qa-organize/src/polyfills.ts 	new file:   qa-organize/src/styles.scss 	new file:   qa-organize/src/test.ts 	new file:   qa-organize/src/tsconfig.app.json 	new file:   qa-organize/src/tsconfig.spec.json 	new file:   qa-organize/src/tslint.json 	new file:   qa-organize/tsconfig.json 	new file:   qa-organize/tslint.json 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.scss 	deleted:    src/App.test.js 	deleted:    src/ScriptRow/Script.css 	deleted:    src/ScriptRow/Script.js 	deleted:    src/ScriptRow/Script.scss 	deleted:    src/ScriptRow/Script.test.js 	deleted:    src/ScriptRow/StatusBar.js 	deleted:    src/assets/accessibility.png 	deleted:    src/assets/android.png 	deleted:    src/assets/chrome.png 	deleted:    src/assets/edge.png 	deleted:    src/assets/firefox.png 	deleted:    src/assets/html_5.png 	deleted:    src/assets/ios.png 	deleted:    src/assets/linux.png 	deleted:    src/assets/safari.png 	deleted:    src/assets/web.png 	deleted:    src/data/ScriptData.js 	deleted:    src/index.css 	deleted:    src/index.js 	deleted:    src/serviceWorker.js

--- src/ScriptRow/Script.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Script from './Script';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Script />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
